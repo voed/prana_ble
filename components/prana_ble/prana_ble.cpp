7e04842c66732709972054de25f60cb613681d94
voed <voed@i.ua>
2022-04-24 18:44:01 +0300

    1

--- components/prana_ble/prana_ble.cpp
+++ components/prana_ble/prana_ble.cpp
@@ -34,7 +34,7 @@ void PranaBLE::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gat
       this->char_handle_ = chr->handle;
       
       
-      this->node_state = esp32_ble_tracker::ClientState::ESTABLISHED;
+      
       
       
       
@@ -52,7 +52,9 @@ void PranaBLE::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gat
         ESP_LOGW(TAG, "No descriptor found for notify of handle 0x%x", param->reg_for_notify.handle);
         break;
       }
-      uint8_t notify_en = 2;
+
+      //this->node_state = esp32_ble_tracker::ClientState::ESTABLISHED;
+      uint8_t notify_en = 1;
       auto status = esp_ble_gattc_write_char_descr(this->parent()->gattc_if, this->parent()->conn_id, descr->handle, sizeof(notify_en),
                                                    &notify_en, ESP_GATT_WRITE_TYPE_RSP, ESP_GATT_AUTH_REQ_NONE);
 
